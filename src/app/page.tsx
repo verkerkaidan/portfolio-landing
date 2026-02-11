28c6e8d3ac60e247b5c57456108ccf07f139f516
verkerkaidan <aidanverkerk@hotmail.com>
2026-02-11 20:07:11 +0100

    Remove tictactoe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,40 +8,32 @@ import { nl } from "../../constants/nl";
 import { PROJECT_LINKS } from "../../constants/project-links";
 
 export default function Home() {
-	const [lang, setLang] = useState<Language>("en");
-	const content = lang === "nl" ? nl : en;
+  const [lang, setLang] = useState<Language>("en");
+  const content = lang === "nl" ? nl : en;
 
-	return (
-		<>
-			<Header title={content.projectsHeader} lang={lang} setLang={setLang} />
-			<ul className="space-y-4 w-full">
-				<ProjectItem
-					href={PROJECT_LINKS.POWERLIFTING_CALENDAR}
-					title={content.powerliftingCalendarTitle}
-					description={content.powerliftingCalendarDescription}
-					tags={["React", "Next.js", "Python", "Supabase"]}
-				/>
-				<ProjectItem
-					href={PROJECT_LINKS.DRAW_SOMETHING}
-					title={content.drawSomethingTitle}
-					description={content.drawSomethingDescription}
-					tags={["React", "Next.js", "Supabase"]}
-				/>
-				<ProjectItem
-					href={PROJECT_LINKS.PORTFOLIO} // TODO: Replace with Tic Tac Toe link
-					title={"Tic Tac Toe [TBD]"}
-					description={
-						"Will be making Tic Tac Toe with live multiplayer functionality and possibly AI opponent functionality!"
-					}
-					tags={["React", "Next.js", "Firebase"]}
-				/>
-				<ProjectItem
-					href={PROJECT_LINKS.PORTFOLIO}
-					title={content.portfolioTitle}
-					description={content.portfolioDescription}
-					tags={["React", "Next.js"]}
-				/>
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <Header title={content.projectsHeader} lang={lang} setLang={setLang} />
+      <ul className="space-y-4 w-full">
+        <ProjectItem
+          href={PROJECT_LINKS.POWERLIFTING_CALENDAR}
+          title={content.powerliftingCalendarTitle}
+          description={content.powerliftingCalendarDescription}
+          tags={["React", "Next.js", "Python", "Supabase"]}
+        />
+        <ProjectItem
+          href={PROJECT_LINKS.DRAW_SOMETHING}
+          title={content.drawSomethingTitle}
+          description={content.drawSomethingDescription}
+          tags={["React", "Next.js", "Supabase"]}
+        />
+        <ProjectItem
+          href={PROJECT_LINKS.PORTFOLIO}
+          title={content.portfolioTitle}
+          description={content.portfolioDescription}
+          tags={["React", "Next.js"]}
+        />
+      </ul>
+    </>
+  );
 }
